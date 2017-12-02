763a4eb1e28364280e1da2e84ae8e7bd5435480d
islamshohidul <shohidul8900586@gmail.com>
2017-12-02 22:13:21 +0800

    Create problem_1.java

---
+++ problem_1.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int sum = 0;
+    for (int i = 3; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    System.out.println(sum);
+		
+	}	
+		
+
+}
